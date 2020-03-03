bed7b8b946087650791d6caefeefd639622e0383
xiaohuihuiCMJ <18882565275@163.com>
2020-03-03 18:03:23 +0800

    创建

---
+++ src/components/dict/indexRenche.js
@@ -0,0 +1,8 @@
+import T from './RencheDictSelectTag.vue'
+
+const RencheDictSelectTag = {
+  install: function (Vue) {
+    Vue.component('RencheDictSelectTag',T);
+  }
+}
+export default RencheDictSelectTag;
