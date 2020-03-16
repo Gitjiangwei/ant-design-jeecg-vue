a910892e63ccc812967d4573436f166bba80f064
JW <15035571514@163.com>
2020-03-16 18:35:05 +0800

    附件校验

--- src/views/work/equipment/modules/pruchaseInfoMode.vue
+++ src/views/work/equipment/modules/pruchaseInfoMode.vue
@@ -275,12 +275,15 @@
               httpurl += this.url.edit;
               method = 'put';
             }
-            let a = this.avatar.charAt(this.avatar.length - 1);
-            debugger;
-            if(a == ",") {
-              this.avatar = this.avatar.substring(0, this.avatar.length - 1);
+            if(this.avatar!=null && this.avatar != "" && this.avatar !=undefined) {
+              let a = this.avatar.charAt(this.avatar.length - 1);
+              debugger;
+              if (a == ",") {
+                this.avatar = this.avatar.substring(0, this.avatar.length - 1);
+              }
             }
             this.model.fileRelId = this.avatar;
+
             debugger;
             var reg = new RegExp("[\\u4E00-\\u9FFF]+","g");
             if(reg.test(this.model.whichCompany)){
