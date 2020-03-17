71622d7214237f6425c5f70679e4548f2d3c8705
JW <15035571514@163.com>
2020-03-17 13:47:19 +0800

    整体页面修改

--- src/components/tools/Logo.vue
+++ src/components/tools/Logo.vue
@@ -2,7 +2,8 @@
   <div class="logo">
     <router-link :to="{name:'dashboard'}">
       <img src="~@/assets/logo.svg" alt="logo">
-      <h1 v-if="showTitle">{{ title }}</h1>
+      <!--<h1 v-if="showTitle">{{ title }}</h1>-->
+      <h1 v-if="showTitle">人车4G子系统</h1>
     </router-link>
   </div>
 </template>
