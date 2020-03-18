98b51e673403cd996e0445634f58212ea9cef7c5
JW <15035571514@163.com>
2020-03-18 18:23:24 +0800

    设备入库

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -9,7 +9,7 @@
 
           <a-form-item
             fieldDecoratorId="username"
-            :fieldDecoratorOptions="{rules: [{ required: true, message: '请输入帐户名或邮箱' }, { validator: this.handleUsernameOrEmail }], validateTrigger: 'change'}">
+            :fieldDecoratorOptions="{rules: [{ required: true, message: '请输入帐户名' }, { validator: this.handleUsernameOrEmail }], validateTrigger: 'change'}">
             <a-input size="large" type="text" placeholder="请输入帐户名">
               <a-icon slot="prefix" type="user" :style="{ color: 'rgba(0,0,0,.25)' }"/>
             </a-input>
